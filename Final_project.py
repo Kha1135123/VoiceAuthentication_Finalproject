199f1827e7c065787aeaab154a8a7a6614346811
Kha1135123 <79006486+Kha1135123@users.noreply.github.com>
2022-06-16 20:15:16 +0700

    Update Final_project.py

--- Final_project.py
+++ Final_project.py
@@ -173,7 +173,7 @@ if st.session_state.sidebar == 'Home':
             if not os.path.exists("audio"):
                 os.makedirs("audio")
             path = os.path.join("audio", uploaded_file.name)
-            st.write("The dir is: %s" %os.listdir("audio")
+            #st.write("The dir is: %s" %os.listdir("audio")
             if_save_audio = save_audio(uploaded_file)
 
             spoken = asr_model.transcribe_file(path)           
